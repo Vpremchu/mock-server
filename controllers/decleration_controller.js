3350c4a3c873f65e6554d8bd17fc5184d1f269b5
Rixt94 <rixt@denhoedt.nl>
2018-06-05 20:56:03 +0200

    set request van de declaration en basis van de test opgezet

--- controllers/decleration_controller.js
+++ controllers/decleration_controller.js
@@ -3,6 +3,8 @@
 //
 
 const db = require('../config/db');
+const ApiError = require('../models/ApiError');
+
 
 module.exports = {
     postDecleration(request, response, next) {
@@ -12,5 +14,28 @@ module.exports = {
         } catch (error) {
             throw(new ApiError(error.toString(), 412))
         }
+    },
+
+    //function to update status in the database
+    setDecleration(req, res, next){
+        try{
+            const mrn = req.parms.mrn || '';
+            const status = req.body.status;
+
+            assert(mrn !== '', 'MRN was not defined or passed as empty');
+            assert(typeof(mrn) === 'string', 'MRN is not of type string');
+            assert(status > 0, 'status not defined or passed as empty');
+
+            db.query('UPDATE declaration SET status=' + status + 'WHERE mrn ="' + mrn + '"', (err, rows, fields) => {
+                if(err){
+                    next(new ApiError(500, err.message));
+                }else{
+                    res.sendStatus(200).json(rows[0]).end();
+                }
+            });
+        } catch (error){
+            next(new ApiError(412, error.message));
+        }
+
     }
-}
+};
--- routes/decleration_routes.js
+++ routes/decleration_routes.js
@@ -8,6 +8,7 @@ let routes = express.Router();
 
 // The router endpoints that we provide
 routes.post('/decleration',decleration_controller.postDecleration);
+routes.set('/decleration/:mrn',decleration_controller.setDecleration);
 
 
 // Exporting the routes so they can be used by the other classes
