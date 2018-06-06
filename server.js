d55a17b084ab737e3376559ed094b02f96f3ffe3
Kelly Premchuen <vpremchu@avans.nl>
2018-06-06 20:57:58 +0200

    Merged

--- server.js
+++ server.js
@@ -12,20 +12,14 @@ const config = require('./config/config.json')
 const db = require('./config/db');
 
 //Include the routes
-<<<<<<< HEAD
-<<<<<<< HEAD
 const declaration_routes = require('./routes/declaration_routes');
 const error_routes = require('./routes/error_routes');
 
 // Include necessary controllers
-=======
 const decleration_routes = require('./routes/declaration_routes');
 const error_routes = require('./routes/error_routes');
->>>>>>> 0bfd3e58aefea90f06bf134be842d48cc9a1804e
-=======
 const decleration_routes = require('./routes/declaration_routes');
 const error_routes = require('./routes/error_routes');
->>>>>>> 0bfd3e58aefea90f06bf134be842d48cc9a1804e
 const ErrorController = require('./controllers/error_controller');
 
 //  Setting up all the necessary variables used by the express library
@@ -42,17 +36,12 @@ app.use(bodyparser.json());
 //  All routing usages
 //
 // Parse all the requests regarding decleration.
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 app.use('/api', declaration_routes);
 
 //Error handling - Endpoint handling routing and final error destination handling
-=======
 app.use('/api', decleration_routes);
->>>>>>> 0bfd3e58aefea90f06bf134be842d48cc9a1804e
-=======
 app.use('/api', decleration_routes);
->>>>>>> 0bfd3e58aefea90f06bf134be842d48cc9a1804e
 app.use('*', error_routes);
 app.use(ErrorController.errorHandling);
 //
