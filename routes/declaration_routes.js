658facb5a481f02c9940c86bd2658d158d4670e1
Aron Cornet <aron@h-cornet.nl>
2018-06-18 12:00:21 +0200

    Fixed file adding route.

--- routes/declaration_routes.js
+++ routes/declaration_routes.js
@@ -9,7 +9,7 @@ let routes = express.Router();
 // The router endpoints that we provide
 routes.post('/declaration',declaration_controller.postDeclaration);
 routes.post('/file', declaration_controller.addFile);
-routes.get('/file/:mrn',declaration_controller.getFile);
+routes.post('/file/:mrn',declaration_controller.addFile);
 routes.put('/declaration/:mrn',declaration_controller.setDeclaration);
 
 // Exporting the routes so they can be used by the other classes
